89db388f26dc8aa1c9704ea8e648415bae76c1bf
amira <amira@localhost.localdomain>
2025-12-03 01:53:03 +0200

    adding more details

--- updatetable.sh
+++ updatetable.sh
@@ -26,8 +26,11 @@ then
 	exit 1
 fi
 
-columns=$(head -n1 "$meta_file" | tr '|' ' ')
-choice=$(zenity --list --title="Columns to choose from" --column="Column" $columns)
+columns=$(sed -n '1p' "$meta_file")
+data_types=$(sed -n '2p' "$meta_file")
+primaryK=$(sed -n '3p' "$meta_file")
+columns_separtated=$(echo "$columns" | tr '|' ' ')
+choice=$(zenity --list --title="Columns to choose from" --column="Column" $columns_seperated)
 if [ $? -ne 0 ] 
 then
 zenity --error --text="NO Columns found in MetaData"
@@ -39,16 +42,20 @@ then
 	zenity --error --text="No Column Choosed"
 	exit 0
 fi
-column_data_type=0
+column_no=$(echo "$columns" | tr '|' '\n' | grep -n "^$choice$" | cut -d: -f1)
+column_data_type=$(echo "$data_types" | cut -d'|' -f"$column_no")
+rows=$(awk -F '|' -v col="$column_no" '{
+print NR " - " $col}' "$data_file")
+
 row=$(zenity --list --title="Available Rows to chooses from" 
---text ="Rows in the table named: $table (Column: $column)"
---column="Row Number - Value" )
+--text="Rows in the table named: $table (Column: $choice)"
+--column="Row Number - Value" $rows)
 
 if [ $? -ne 0 ] || [ -z "$row" ]
 then
 	exit 0
 fi
-
+row_no=$(echo "$row" | cut -d' ' -f1)
 updated_value=$(zenity --entry --title="updating" --text="Enter new Value")
 if [[ -z "$updated_value" ]]
 then
@@ -61,11 +68,11 @@ then
 		zenity --error --text="invalid input, expected and int"
 		exit 0
 	fi
-#elif [[ "$col_data_type" == "varchar" ]] 
+
 # primary_key_validation
 
 
-awk -F '|' -v col="$get_column" -v val="$updated_value" '$col !=val' "$data_file" > tmp && mv tmp "$data_file"
+awk -F '|' -v col="$column_no" -v val="$updated_value" -v row="$row_no"'BEGIN { OFS="|"} NR == row { $col = val } { print }' "$data_file" > tmp && mv tmp "$data_file"
 zenity --info --text="value updated succesfully"
 
 
