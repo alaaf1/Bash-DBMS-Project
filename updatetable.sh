ac1b6b275f86c6aa6a296ff455ed17ad49943568
amira <amira@localhost.localdomain>
2025-12-03 02:11:19 +0200

    updated

--- updatetable.sh
+++ updatetable.sh
@@ -29,8 +29,8 @@ fi
 columns=$(sed -n '1p' "$meta_file")
 data_types=$(sed -n '2p' "$meta_file")
 primaryK=$(sed -n '3p' "$meta_file")
-columns_separtated=$(echo "$columns" | tr '|' ' ')
-choice=$(zenity --list --title="Columns to choose from" --column="Column" $columns_seperated)
+columns_separated=$(echo "$columns" | tr '|' ' ')
+choice=$(zenity --list --title="Columns to choose from" --column="Column" $columns_separated)
 if [ $? -ne 0 ] 
 then
 zenity --error --text="NO Columns found in MetaData"
@@ -44,12 +44,10 @@ then
 fi
 column_no=$(echo "$columns" | tr '|' '\n' | grep -n "^$choice$" | cut -d: -f1)
 column_data_type=$(echo "$data_types" | cut -d'|' -f"$column_no")
-rows=$(awk -F '|' -v col="$column_no" '{
-print NR " - " $col}' "$data_file")
-
 row=$(zenity --list --title="Available Rows to chooses from" 
 --text="Rows in the table named: $table (Column: $choice)"
---column="Row Number - Value" $rows)
+--column="Row Number - Value" $(awk -F '|' -v col="$column_no" '{ print NR " -
+" $col }' "$data_file"))
 
 if [ $? -ne 0 ] || [ -z "$row" ]
 then
@@ -61,7 +59,7 @@ if [[ -z "$updated_value" ]]
 then
 	zenity --error --text="value should not be empty"
 	exit 0
-elif [[ "$col_data_type" == "int" ]] 
+elif [[ "$column_data_type" == "int" ]] 
 then
 	if [[ ! "$updated_value" =~ ^[0-9]+$ ]]
 	then
